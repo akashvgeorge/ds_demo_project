80117290f1add6839fed949aaa0206d3ef28adc3
Akash George <akashgeorge@Akashs-MacBook-Air.local>
2026-05-15 12:19:53 +0530

    update

---
+++ src/helper.py
@@ -0,0 +1,10 @@
+import yaml
+from os.path import join
+
+CONFIG_PATH = "config"
+config_name = "config.yaml"
+
+def load_config(config_name: str = config_name):
+    with open(join(CONFIG_PATH, config_name), "r") as f:
+        config = yaml.safe_load(f)
+    return config
